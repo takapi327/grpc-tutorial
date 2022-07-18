dff436dc074c01934512610056e14cf8f9812f62
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-07-18 17:14:12 +0900

    Create UnaryServerInterceptor

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,11 +5,8 @@ import (
 	"errors"
 	"fmt"
 	hellopb "github.com/takapi327/grpc-tutorial/pkg/grpc"
-	"google.golang.org/genproto/googleapis/rpc/errdetails"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/reflection"
-	"google.golang.org/grpc/status"
 	"io"
 	"log"
 	"net"
@@ -25,7 +22,9 @@ func main() {
 		panic(err)
 	}
 
-	s := grpc.NewServer()
+	s := grpc.NewServer(
+		grpc.UnaryInterceptor(myUnaryServerInterceptor1),
+	)
 
 	hellopb.RegisterGreetingServiceServer(s, NewMyServer())
 
@@ -48,15 +47,15 @@ type myServer struct {
 }
 
 func (s *myServer) Hello(ctx context.Context, req *hellopb.HelloRequest) (*hellopb.HelloResponse, error) {
-	//return &hellopb.HelloResponse{
-	//	Message: fmt.Sprintf("Hello, %s!", req.GetName()),
-	//}, nil
-	stat := status.New(codes.Unknown, "unknown error occurred")
-	stat, _ = stat.WithDetails(&errdetails.DebugInfo{
-		Detail: "detail reaseon of err",
-	})
-	err := stat.Err()
-	return nil, err
+	return &hellopb.HelloResponse{
+		Message: fmt.Sprintf("Hello, %s!", req.GetName()),
+	}, nil
+	//stat := status.New(codes.Unknown, "unknown error occurred")
+	//stat, _ = stat.WithDetails(&errdetails.DebugInfo{
+	//	Detail: "detail reaseon of err",
+	//})
+	//err := stat.Err()
+	//return nil, err
 }
 
 func (s *myServer) HelloServerStream(req *hellopb.HelloRequest, stream hellopb.GreetingService_HelloServerStreamServer) error {
---
+++ cmd/server/unaryInterceptor.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"context"
+	"google.golang.org/grpc"
+	"log"
+)
+
+func myUnaryServerInterceptor1(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+	log.Println("[pre] my unary server interceptor 1: ", info.FullMethod)
+	res, err := handler(ctx, req)
+	log.Println("[post] my unary server interceptor 1: ", res)
+	return res, err
+}
