e1bea61d6565e86cc26672524aa6135374bb55ed
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-07-18 13:42:29 +0900

    Create server

---
+++ cmd/server/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+)
+
+func main() {
+	port := 8080
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		panic(err)
+	}
+
+	s := grpc.NewServer()
+
+	go func() {
+		log.Printf("start gRPC server port: %v", port)
+		s.Serve(listener)
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	log.Println("stopping gRPC server...")
+	s.GracefulStop()
+}
