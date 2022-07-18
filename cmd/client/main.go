b771a6c95cc41b441ce029ce2a82decbe757bfa7
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2022-07-18 15:30:10 +0900

    Add gRPC Error Status code

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -6,8 +6,10 @@ import (
 	"errors"
 	"fmt"
 	hellopb "github.com/takapi327/grpc-tutorial/pkg/grpc"
+	_ "google.golang.org/genproto/googleapis/rpc/errdetails"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/status"
 	"io"
 	"log"
 	"os"
@@ -77,7 +79,13 @@ func Hello() {
 	}
 	res, err := client.Hello(context.Background(), req)
 	if err != nil {
-		fmt.Println(err)
+		if stat, ok := status.FromError(err); ok {
+			fmt.Printf("code: %s\n", stat.Code())
+			fmt.Printf("message: %s\n", stat.Message())
+			fmt.Printf("details: %s\n", stat.Details())
+		} else {
+			fmt.Println(err)
+		}
 	} else {
 		fmt.Println(res.GetMessage())
 	}
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,8 +5,11 @@ import (
 	"errors"
 	"fmt"
 	hellopb "github.com/takapi327/grpc-tutorial/pkg/grpc"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/grpc/status"
 	"io"
 	"log"
 	"net"
@@ -45,9 +48,15 @@ type myServer struct {
 }
 
 func (s *myServer) Hello(ctx context.Context, req *hellopb.HelloRequest) (*hellopb.HelloResponse, error) {
-	return &hellopb.HelloResponse{
-		Message: fmt.Sprintf("Hello, %s!", req.GetName()),
-	}, nil
+	//return &hellopb.HelloResponse{
+	//	Message: fmt.Sprintf("Hello, %s!", req.GetName()),
+	//}, nil
+	stat := status.New(codes.Unknown, "unknown error occurred")
+	stat, _ = stat.WithDetails(&errdetails.DebugInfo{
+		Detail: "detail reaseon of err",
+	})
+	err := stat.Err()
+	return nil, err
 }
 
 func (s *myServer) HelloServerStream(req *hellopb.HelloRequest, stream hellopb.GreetingService_HelloServerStreamServer) error {
